2231a4a45c61488460ca458c6c9ce3887151f083
gijslaarman <gijslaarman@gmail.com>
2020-11-15 22:16:29 +0100

    add: updated Error page with animation

---
+++ components/AnimationPlayer.vue
@@ -0,0 +1,29 @@
+<template>
+  <lottie :options="lottieOptions" @animCreated="handleAnimation" />
+</template>
+
+<script>
+import lottie from 'vue-lottie/src/lottie.vue'
+import * as animationData from '@/static/animations/puzzle'
+
+export default {
+  components: {
+    lottie,
+  },
+
+  data() {
+    return {
+      anim: null, // for saving the reference to the animation
+      lottieOptions: { animationData: animationData.default },
+    }
+  },
+
+  methods: {
+    handleAnimation(anim) {
+      this.anim = anim
+    },
+  },
+}
+</script>
+
+<style></style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,22 +1,61 @@
 <template>
-  <section class="container" style="padding-top: 10rem">
-    <h1 v-if="error.statusCode === 404">{{ error.message }}</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+  <section class="container error">
+    <div class="animation">
+      <AnimationPlayer />
+    </div>
+
+    <section class="error-message">
+      <h1 v-if="error.statusCode === 404">{{ error.message }}</h1>
+      <h1 v-else>An error occurred</h1>
+
+      <Button href="/">Click here to go back</Button>
+    </section>
   </section>
 </template>
 
 <script>
+import AnimationPlayer from '@/components/AnimationPlayer'
+import Button from '@/components/ui/Button'
+
 export default {
+  components: { AnimationPlayer, Button },
   props: {
     error: {
-      type: String,
+      type: Object,
       default: () => ({
         statusCode: 404,
         message: 'Page not found',
       }),
     },
   },
+
+  scrollToTop: true,
   layout: 'error', // you can set a custom layout for the error page
 }
 </script>
+
+<style lang="scss">
+section.error {
+  padding-top: 6rem;
+}
+.animation {
+  width: 100%;
+  margin: 0 auto 3rem;
+}
+
+.error-message {
+  margin-bottom: 100px;
+}
+
+@include for-tablet-landscape-up {
+  .button {
+    display: inline-block;
+  }
+  section.error {
+    padding-top: 10rem;
+  }
+  .animation {
+    width: 50%;
+  }
+}
+</style>
