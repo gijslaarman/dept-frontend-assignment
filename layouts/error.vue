f282c09eb143f15641feb3261b6274e3e93e484d
gijslaarman <gijslaarman@gmail.com>
2020-11-15 15:54:50 +0100

    fix: error not rendering right

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container" style="padding-top: 10rem">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-if="error.statusCode === 404">{{ error.message }}</h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/">Home page</NuxtLink>
   </section>
@@ -11,7 +11,10 @@ export default {
   props: {
     error: {
       type: String,
-      default: 'Error',
+      default: () => ({
+        statusCode: 404,
+        message: 'Page not found',
+      }),
     },
   },
   layout: 'error', // you can set a custom layout for the error page
