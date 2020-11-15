f68c309283865e1ebe4f67083620894f94b39dd3
gijslaarman <gijslaarman@gmail.com>
2020-11-15 22:16:06 +0100

    hotfix: forgot the dropdown

--- components/ui/Dropdown.vue
+++ components/ui/Dropdown.vue
@@ -3,15 +3,14 @@
     <label>
       {{ label }}
     </label>
-    <select>
-      <option
-        v-for="option in options"
-        :key="option.value"
-        :value="option.value"
-      >
-        {{ option.name }}
-      </option>
-    </select>
+    <div class="select-wrapper">
+      <select ref="select" @change="emitNewValue">
+        <option v-for="option in options" :key="option" :value="option">
+          {{ option }}
+        </option>
+      </select>
+      <svg-icon class="dropdown-arrow" name="triangle" />
+    </div>
   </div>
 </template>
 
@@ -27,7 +26,46 @@ export default {
       default: () => [],
     },
   },
+
+  methods: {
+    emitNewValue() {
+      const newValue = this.$refs.select.value
+      this.$emit('valueChange', newValue)
+    },
+  },
 }
 </script>
 
-<style></style>
+<style lang="scss">
+select {
+  cursor: pointer;
+  display: inline-block;
+  font-family: 'Teko';
+  font-size: 1.875rem;
+  font-weight: 500;
+  text-transform: capitalize;
+  line-height: 1em;
+  height: 0.875em;
+  color: $black;
+  border: none;
+  border-radius: 0px;
+  background-color: transparent;
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+}
+
+.dropdown-arrow {
+  display: inline-block;
+  transform: rotate(90deg);
+  fill: $black;
+  width: 7px;
+  height: 9px;
+  margin-bottom: 7px;
+}
+
+.select-wrapper {
+  display: inline-block;
+  border-bottom: 1px solid $black;
+}
+</style>
